37360cf3ca8c4f0af7727bfc1250160ff32dd7bc
pk34 <pk34@live.com.mx>
2025-10-17 14:59:05 -0600

    fix: constrain search icon width

--- apps/storefront/src/components/filter/B3FilterSearch.tsx
+++ apps/storefront/src/components/filter/B3FilterSearch.tsx
@@ -76,7 +76,7 @@ function B3FilterSearch({
         viewBox="0 0 20 22"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
-        sx={{ flexShrink: 0 }}
+        sx={{ flexShrink: 0, width: '20px', height: '20px' }}
       >
         <path
           fillRule="evenodd"
