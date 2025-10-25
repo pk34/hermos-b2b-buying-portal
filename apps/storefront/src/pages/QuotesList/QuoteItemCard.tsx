c9e34743cb0219ec892b7953ad9f7c710a8c9e14
pk34 <pk34@live.com.mx>
2025-10-25 15:10:57 -0600

    Fix quote card theme typing

--- apps/storefront/src/pages/QuotesList/QuoteItemCard.tsx
+++ apps/storefront/src/pages/QuotesList/QuoteItemCard.tsx
@@ -1,4 +1,4 @@
-import styled from '@emotion/styled';
+import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
